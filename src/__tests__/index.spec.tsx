1bf8b2757144ec01cff282e235fb504ecd1d340c
Tom Lackemann <tom@astraltabletop.com>
2019-12-06 07:03:48 -0500

    fix: add single test

---
+++ src/__tests__/index.spec.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import { useMenu } from '../'
+
+describe('react-menuit', () => {
+  it('exposes "useMenu" hook', () => {
+    expect(typeof useMenu).not.toBe('undefined')
+  })
+})
